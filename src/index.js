46cdb6858397d8a2f1400206ba71455ea7d8be3c
Ivan Coronado Moreno <ivacormor@gmail.com>
2018-10-26 16:32:30 +0200

    add redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux'
 import { Navigator } from './navigator'
 import { theme } from './theme'
+import { configureStore } from './store'
 
 global.XMLHttpRequest = global.originalXMLHttpRequest
     ? global.originalXMLHttpRequest
     : global.XMLHttpRequest
 
+const store = configureStore()
+
 export class Root extends Component {
     render() {
         return (
-            <ThemeProvider theme={theme}>
-                <Navigator />
-            </ThemeProvider>
+            <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                    <Navigator />
+                </ThemeProvider>
+            </Provider>
         )
     }
 }
